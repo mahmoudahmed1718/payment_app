85c95e2f903fbd9a933aad49c2bc714123aaad6d
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-31 06:51:59 +0300

    create my cart view body

--- lib/features/checkout/presentation/view/my_cart_view.dart
+++ lib/features/checkout/presentation/view/my_cart_view.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:payment_app/core/widgets/custom_app_bar.dart';
+import 'package:payment_app/features/checkout/presentation/view/widgets/my_cart_view_body.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: buildAppBar(title: 'My Cart'),
+      body: MyCartViewBody(),
+    );
   }
 }
---
+++ lib/features/checkout/presentation/view/widgets/my_cart_view_body.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyCartViewBody extends StatelessWidget {
+  const MyCartViewBody({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
